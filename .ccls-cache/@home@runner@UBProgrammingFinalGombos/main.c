c8b30a03db8fab92a3eaffd18006235cbb542b9d
PumpkintheIII <50267286+PumpkintheIII@users.noreply.github.com>
2023-05-22 13:12:34 +0000

    added comments

--- .ccls-cache/@home@runner@UBProgrammingFinalGombos/main.c
+++ .ccls-cache/@home@runner@UBProgrammingFinalGombos/main.c
@@ -3,20 +3,24 @@
   Quadratic Formula Calculator
   Maxwell Gombos
 */
-#include <stdio.h>
+#include <stdio.h> //import input and output functions
 
-int sqr(int term) {
-  int index, root;
+int sqr(int term) { //define sqr funtion
+  int index, root; //define variables
   if (term > 0) {
+    //if the term is positive, calculate the whole square root of the term
     for (index = 1; index <= term; index++) {
+      //find each factor of the term, and check if it is the square root of the term
       if (term%index == 0) {
         if (term/index == index) {
+          //if it is the square root of the term, return the number
           root = index;
           return root;
         }
       }
     }
   }
+  //if there is no whole square root, return -1
   return -1;
 }
 
@@ -32,44 +36,49 @@ int main(void) {
     part4: 2a
     term: b^2 - 4ac
   */
-  int a, b, c, term, root;
-  int numeratorA, numeratorB, denominator;
-  term = 0;
+  int a, b, c, term, root; //define variables as integer
+  int numeratorA, numeratorB, denominator;  //define variables as integer
+  term = 0; //intitialize term variable
 
-  printf("a: ");
-  scanf("%d", &a);
-  printf("b: ");
-  scanf("%d", &b);
-  printf("c: ");
-  scanf("%d", &c);
+  printf("a: "); //ask user for input
+  scanf("%d", &a); //accept user input
+  printf("b: "); //ask user for input
+  scanf("%d", &b); //accept user input
+  printf("c: "); //ask user for input
+  scanf("%d", &c); //accept user input
 
+  //find the term of the square root (b^2-4ac)
   term = b * b;
   term += (-4 * a) * c;
   printf("%d", term);
 
-  root = sqr(term);
+  root = sqr(term); //call the sqr function
   if (root != -1) {
+    //if the square root function found a whole root, calculate the quadratic formula
     printf("\n%d\n", root);
-    numeratorA = (b * -1) + root;
-    numeratorB = (b * -1) - root;
-    denominator = (2 * a);
-    printf("\n%d/%d, %d/%d\n", numeratorA, denominator, numeratorB, denominator);
+    numeratorA = (b * -1) + root; //find the numberator of -b + sqrt(b^2 - 4ac)
+    numeratorB = (b * -1) - root; //find the numberator of -b - sqrt(b^2 - 4ac)
+    denominator = (2 * a); //find the denominator
+    printf("\n%d/%d, %d/%d\n", numeratorA, denominator, numeratorB, denominator); //print the output
   }
   else {
+    //if the sqr function did not find a whole root, check if it is an imaginary number or parial root:
     if (term < 0) {
-      term *= -1;
+      //if it is an imaginary number, find the output of the formula using imaginary numbers to simplify the root
+      term *= -1; //update term variable
       printf("\ni sqrt(%d)\n", term);
-      numeratorA = (b * -1);
-      numeratorB = (b * -1);
-      denominator = (2 * a);
-      printf("\n%d + i sqrt(%d)/%d, %d - i sqrt(%d)/%d\n", numeratorA, term, denominator, numeratorB, term, denominator);
+      numeratorA = (b * -1); //find the numberator of -b + sqrt(b^2 - 4ac)
+      numeratorB = (b * -1); //find the numberator of -b - sqrt(b^2 - 4ac)
+      denominator = (2 * a); //find the denominator
+      printf("\n%d + i sqrt(%d)/%d, %d - i sqrt(%d)/%d\n", numeratorA, term, denominator, numeratorB, term, denominator); //print the output
     }
     else {
+      //if it is a partial root, find the output of the formula using partial root
       printf("\nsqrt(%d)\n", term);
-      numeratorA = (b * -1);
-      numeratorB = (b * -1);
-      denominator = (2 * a);
-      printf("\n%d + sqrt(%d)/%d, %d - sqrt(%d)/%d\n", numeratorA, term, denominator, numeratorB, term, denominator);
+      numeratorA = (b * -1); //find the numberator of -b + sqrt(b^2 - 4ac)
+      numeratorB = (b * -1); //find the numberator of -b - sqrt(b^2 - 4ac)
+      denominator = (2 * a); //find the denominator
+      printf("\n%d + sqrt(%d)/%d, %d - sqrt(%d)/%d\n", numeratorA, term, denominator, numeratorB, term, denominator); //print the output
     }
   }
   
