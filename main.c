89eb109e4ec9818edc98c69182e6e1d76185b7a9
PumpkintheIII <50267286+PumpkintheIII@users.noreply.github.com>
2023-05-23 12:31:18 +0000

    Iteration 4, simplify output fractions

--- main.c
+++ main.c
@@ -36,9 +36,11 @@ int main(void) {
     part4: 2a
     term: b^2 - 4ac
   */
-  int a, b, c, term, root; //define variables as integer
-  int numeratorA, numeratorB, denominator;  //define variables as integer
+  int a, b, c, term, root, index; //define variables as integer
+  int numeratorA, numeratorB, denominatorA, denominatorB, outA, outB;  //define variables as integer
   term = 0; //intitialize term variable
+  outA = 0;
+  outB = 0;
 
   printf("a: "); //ask user for input
   scanf("%d", &a); //accept user input
@@ -50,35 +52,89 @@ int main(void) {
   //find the term of the square root (b^2-4ac)
   term = b * b;
   term += (-4 * a) * c;
-  printf("%d", term);
 
   root = sqr(term); //call the sqr function
   if (root != -1) {
     //if the square root function found a whole root, calculate the quadratic formula
-    printf("\n%d\n", root);
     numeratorA = (b * -1) + root; //find the numberator of -b + sqrt(b^2 - 4ac)
     numeratorB = (b * -1) - root; //find the numberator of -b - sqrt(b^2 - 4ac)
-    denominator = (2 * a); //find the denominator
-    printf("\n%d/%d, %d/%d\n", numeratorA, denominator, numeratorB, denominator); //print the output
+    denominatorA = (2 * a); //find the denominator
+    denominatorB = (2 * a); //find the denominator
+    if (numeratorA > denominatorA) {
+      if (numeratorA%denominatorA == 0) {
+        numeratorA = numeratorA/denominatorA;
+        denominatorA = 1;
+      }
+      else {
+        while (numeratorA > denominatorA) {
+          numeratorA -= denominatorA;
+          outA += 1;
+        }
+      }
+    }
+    else {
+      for (index = 1; index < denominatorA; index++) {
+        if (denominatorA%index == 0) {
+          if (numeratorA%index == 0) {
+            numeratorA = numeratorA/index;
+            denominatorA = denominatorA/index;
+          }
+        }
+      }
+    }
+    if (numeratorB > denominatorB) {
+      if (numeratorB%denominatorB == 0) {
+        numeratorB = numeratorB/denominatorB;
+        denominatorB = 1;
+      }
+      else {
+        while (numeratorB > denominatorB) {
+          numeratorB -= denominatorB;
+          outB += 1;
+        }
+      }
+    }
+    else {
+      for (index = 1; index < denominatorB; index++) {
+        if (denominatorB%index == 0) {
+          if (numeratorB%index == 0) {
+            numeratorB = numeratorB/index;
+            denominatorB = denominatorB/index;
+          }
+        }
+      }
+    }
+    if (outA != 0 && outB == 0) {
+      printf("\n%d %d/%d, %d/%d\n", outA, numeratorA, denominatorA, numeratorB, denominatorB); //print the output
+    }
+    else if (outA == 0 && outB != 0) {
+      printf("\n%d/%d, %d %d/%d\n", numeratorA, denominatorA, outB, numeratorB, denominatorB); //print the output
+    }
+    else if (outA != 0 && outB != 0) {
+      printf("\n%d %d/%d, %d %d/%d\n", outA, numeratorA, denominatorA, outB, numeratorB, denominatorB); //print the output
+    }
+    else {
+      printf("\n%d/%d, %d/%d\n", numeratorA, denominatorA, numeratorB, denominatorB); //print the output
+    }
   }
   else {
     //if the sqr function did not find a whole root, check if it is an imaginary number or parial root:
     if (term < 0) {
       //if it is an imaginary number, find the output of the formula using imaginary numbers to simplify the root
       term *= -1; //update term variable
-      printf("\ni sqrt(%d)\n", term);
       numeratorA = (b * -1); //find the numberator of -b + sqrt(b^2 - 4ac)
       numeratorB = (b * -1); //find the numberator of -b - sqrt(b^2 - 4ac)
-      denominator = (2 * a); //find the denominator
-      printf("\n%d + i sqrt(%d)/%d, %d - i sqrt(%d)/%d\n", numeratorA, term, denominator, numeratorB, term, denominator); //print the output
+      denominatorA = (2 * a); //find the denominator
+      denominatorB = (2 * a); //find the denominator
+      printf("\n%d + i sqrt(%d)/%d, %d - i sqrt(%d)/%d\n", numeratorA, term, denominatorA, numeratorB, term, denominatorB); //print the output
     }
     else {
       //if it is a partial root, find the output of the formula using partial root
-      printf("\nsqrt(%d)\n", term);
       numeratorA = (b * -1); //find the numberator of -b + sqrt(b^2 - 4ac)
       numeratorB = (b * -1); //find the numberator of -b - sqrt(b^2 - 4ac)
-      denominator = (2 * a); //find the denominator
-      printf("\n%d + sqrt(%d)/%d, %d - sqrt(%d)/%d\n", numeratorA, term, denominator, numeratorB, term, denominator); //print the output
+      denominatorA = (2 * a); //find the denominator
+      denominatorB = (2 * a); //find the denominator
+      printf("\n%d + sqrt(%d)/%d, %d - sqrt(%d)/%d\n", numeratorA, term, denominatorA, numeratorB, term, denominatorB); //print the output
     }
   }
   
